c0532f632952e7ef816682562d18004d8cc95816
rumi <pouya.zm83@gmail.com>
2025-12-17 00:15:24 +0330

    customization admin

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from todo.models import Task
 
-# Register your models here.
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ["title", "user", "priority", "is_done", "created_at","due_date"]
+    list_filter = ["priority", "is_done"]
+    search_fields = ["title", "description"]
+
+admin.site.register(Task, TaskAdmin)
--- todo/models.py
+++ todo/models.py
@@ -1,34 +1,38 @@
 from django.db import models
 from django.contrib.auth.models import User
-# Create your models here.
-
 
 class Task(models.Model):
+    # انتخاب‌ها برای اولویت (ترتیب مهم است - اول تعریف شود)
+    class PriorityChoices(models.TextChoices):
+        LOW = 'low', 'Low'
+        MEDIUM = 'medium', 'Medium'
+        HIGH = 'high', 'High'
+    
+    # فیلدهای مدل
     user = models.ForeignKey(
         User, 
         on_delete=models.CASCADE,
         related_name='tasks',
-        verbose_name='کاربر'
+        verbose_name='user'
     )
-    class PriorityChoices(models.TextChoices):
-        LOW = 'low'
-        MEDIUM = 'medium'
-        HIGH = 'high'
-
+    title = models.CharField(max_length=225, verbose_name='title')
+    description = models.TextField(blank=True, verbose_name='description')
+    is_done = models.BooleanField(default=False, verbose_name='is done')
+    
     priority = models.CharField(
         max_length=10,
         choices=PriorityChoices.choices,
         default=PriorityChoices.MEDIUM,
-        verbose_name='اولویت'
+        verbose_name='priority'
     )
-    title = models.CharField(max_length=225)
-    description = models.TextField(blank=True)
-    id_done= models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    #priority = models.Choices()
-     
+    
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='created at')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='updated at')
+    due_date = models.DateTimeField(null=True, blank=True, verbose_name='due date')    # متا دیتا
+    class Meta:
+        ordering = ['-created_at']  # جدیدترین اول
+        verbose_name = 'Task'
+        verbose_name_plural = 'Tasks'
+    
     def __str__(self):
-        return self.name
-
- 
+        return f"{self.title} - {self.user.username}"  # اینجا اصلاح شد
