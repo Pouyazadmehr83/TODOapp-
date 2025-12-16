9835a396efe5f305c072b4172b8caf9fc87a59c7
rumi <pouya.zm83@gmail.com>
2025-12-17 03:03:58 +0330

    home page tmp with bootstrp and show data

--- todo/urls.py
+++ todo/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import home_view
+from .views import home_view,details
 
 app_name = "todo"
 
 urlpatterns = [
     path('', home_view, name="home"),
+    path('details/<int:id>', views.details, name='details'),
 
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import Task
+
 
 @login_required
 def home_view(request):
-    return render(request, "todo/home.html")
+    task= Task.objects.all().values()
+    context = {
+    'task': task
+  }
+    return render(request, "todo/home.html",context)
